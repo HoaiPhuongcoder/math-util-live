2dc8cabea44ff90abf884c26ba63719d1bfa8e55
HoaiPhuongcoder <nhphuong203204@gmail.com>
2023-11-10 09:09:04 +0700

    Lan thu 2 lam chuyen ay - quen roi

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
         System.out.println("6! = 720; actual: " + MathUtil.getFactorial(6));
+        //THEM CODE SAU LAN DAU TIEN LAM CHUYEN AY- CODE LEN SEVER 5:35 10/11/2023
+        System.out.println("3! = 6; actual: " + MathUtil.getFactorial(3));
+        System.out.println("1! = 1; actual: " + MathUtil.getFactorial(1));
+        System.out.println("0! = 1; actual: " + MathUtil.getFactorial(0));
     }
 }
